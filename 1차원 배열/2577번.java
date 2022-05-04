2d6d15d721214e549d7dc8ad700df2febc9f4e00
KIM-SeonMi <103207266+KIM-SeonMi@users.noreply.github.com>
2022-05-04 16:12:30 +0900

    BaekJoon 학습

---
+++ 1차원 배열/2577번.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int num = A*B*C;
+		
+		int[] arr = new int[10];
+		
+		while (num != 0) {
+			arr[num%10]++;
+			num /= 10;
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(arr[i]);
+		}
+	}		
+}
